caeb41045a1a3bc12a4c7c9f0f0ded0e3a1f6025
TanveerKalluru <146471689+TanveerKalluru@users.noreply.github.com>
2025-12-25 10:50:36 +0530

    Update App.js

--- Expense-Tracker-App-main/frontend/src/App.js
+++ Expense-Tracker-App-main/frontend/src/App.js
@@ -20,7 +20,7 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/setAvatar" element={<SetAvatar />} />
-        
+        </Routes>
       </BrowserRouter>
       </div>
   )
